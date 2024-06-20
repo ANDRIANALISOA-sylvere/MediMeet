300c24fe1cfbb45f5042d7a4bc429239fbf91938
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-06-20 16:47:10 +0200

    login

--- api/controllers/User.controller.js
+++ api/controllers/User.controller.js
@@ -42,10 +42,51 @@ const Register = async (req, res) => {
         }
       }
     );
-  } catch (error) {}
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
 };
 
+const Login = async (req, res) => {
+  const { email, password } = req.body;
 
-module.exports={
-    Register
-}
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 360000,
+      },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+module.exports = {
+  Register,
+  Login
+};
--- api/routes/User.route.js
+++ api/routes/User.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { Register } = require("../controllers/User.controller");
+const { Register, Login } = require("../controllers/User.controller");
 const router = express.Router();
 
 router.post("/register", Register);
+router.post("/login", Login);
 
 module.exports = router;
