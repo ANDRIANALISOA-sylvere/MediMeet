0642d510a65ea12c4f38f38c64246047f9459ac2
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-07-02 11:54:39 +0200

    add poppins fonts

--- clients/app/index.tsx
+++ clients/app/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Input,
   ApplicationProvider,
@@ -9,13 +9,10 @@ import {
   Icon,
 } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
-import { StyleSheet } from "react-native";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import SplashScreen from "@/components/screen/SplashScreen";
 
 const App = (): React.ReactElement => {
-  const [value, setValue] = React.useState("");
-
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
@@ -25,15 +22,5 @@ const App = (): React.ReactElement => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  button: {
-    margin: 2,
-  },
-});
 
 export default App;
--- clients/components/screen/SplashScreen.tsx
+++ clients/components/screen/SplashScreen.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useEffect } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScree from "expo-splash-screen";
 
 function SplashScreen() {
+  const [loaded, error] = useFonts({
+    Poppins: require("../../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScree.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.welcomeText}>Bienvenue sur</Text>
         <Text style={styles.welcomeText}>MediMeet</Text>
       </View>
-      <Image source={require('../../assets/images/doctor.png')} style={styles.image} />
+      <Image
+        source={require("../../assets/images/doctor.png")}
+        style={styles.image}
+      />
     </View>
   );
 }
@@ -16,19 +35,20 @@ function SplashScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00BFA6',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    backgroundColor: "#00BFA6",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   textContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     left: 20,
-    marginTop:50
+    marginTop: 50,
   },
   welcomeText: {
     fontSize: 36,
-    color: '#fff',
+    color: "#fff",
+    fontFamily: "Poppins",
   },
   image: {
     width: 300,
