a7375607dd929f4b21a1f721a40a0646acf7d1c9
ANDRIANALISOA-sylvere <josephinsylvere@gmail.com>
2024-07-13 01:19:01 +0300

    update MainNavigation

--- clients/components/navigation/MainNavigation.tsx
+++ clients/components/navigation/MainNavigation.tsx
@@ -30,102 +30,108 @@ function HomeStack() {
 
 function TabNavigation() {
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: "#00BFA6",
-        tabBarStyle: { paddingVertical: 10 },
-        tabBarLabelStyle: { fontSize: 10, paddingVertical: 5 },
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeStack}
-        options={{
-          tabBarLabel: "Accueil",
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Icon
-              name={focused ? "home" : "home-outline"}
-              fill={color}
-              style={{ width: size, height: size }}
-            />
-          ),
+    <View style={styles.container}>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: "#00BFA6",
+          tabBarInactiveTintColor: "white",
+          tabBarStyle: {
+            backgroundColor: "#003366",
+            borderRadius: 50,
+            margin: 10,
+          },
         }}
-      />
-      <Tab.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{
-          tabBarLabel: "Messages",
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Icon
-              name={focused ? "message-square" : "message-square-outline"}
-              fill={color}
-              style={{ width: size, height: size }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Appointments"
-        component={AppointmentScreen}
-        options={{
-          tabBarLabel: () => null,
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={[
-                styles.ctaButton,
-                {
-                  backgroundColor: focused ? "#00BFA6" : "white",
-                  borderColor: focused ? "#00BFA6" : "rgba(224, 224, 224, 0)",
-                  borderWidth: 1,
-                },
-              ]}
-            >
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({ color, size, focused }) => (
               <Icon
-                name={focused ? "calendar" : "calendar-outline"}
-                fill={focused ? "white" : "#8e8e8e"}
-                style={styles.appointmentIcon}
+                name={focused ? "home" : "home-outline"}
+                fill={color}
+                style={{ width: size, height: size }}
               />
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={NotificationScreen}
-        options={{
-          tabBarLabel: "Notifications",
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Icon
-              name={focused ? "bell" : "bell-outline"}
-              fill={color}
-              style={{ width: size, height: size }}
-            />
-          ),
-          tabBarBadge: 3,
-        }}
-      />
-      <Tab.Screen
-        name="Account"
-        component={AccountScreen}
-        options={{
-          tabBarLabel: "Compte",
-          headerShown: false,
-          tabBarIcon: ({ color, size, focused }) => (
-            <Icon
-              name={focused ? "person" : "person-outline"}
-              fill={color}
-              style={{ width: size, height: size }}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon
+                name={focused ? "message-square" : "message-square-outline"}
+                fill={color}
+                style={{ width: size, height: size }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Appointments"
+          component={AppointmentScreen}
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={[
+                  styles.ctaButton,
+                  {
+                    backgroundColor: focused ? "#00BFA6" : "#CCF1E2",
+                    borderColor: focused ? "#00BFA6" : "rgba(224, 224, 224, 0)",
+                    borderWidth: 1,
+                  },
+                ]}
+              >
+                <Icon
+                  name={focused ? "calendar" : "calendar-outline"}
+                  fill={focused ? "white" : "#003366"}
+                  style={styles.appointmentIcon}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={NotificationScreen}
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon
+                name={focused ? "bell" : "bell-outline"}
+                fill={color}
+                style={{ width: size, height: size }}
+              />
+            ),
+            tabBarBadge: 3,
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={AccountScreen}
+          options={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon
+                name={focused ? "person" : "person-outline"}
+                fill={color}
+                style={{ width: size, height: size }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </View>
   );
 }
 
@@ -168,13 +174,17 @@ function MainNavigation() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
   ctaButton: {
     width: 60,
     height: 60,
     borderRadius: 30,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 50,
+    marginBottom: 60,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 2,
